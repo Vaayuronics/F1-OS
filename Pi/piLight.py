88f94320de097025b7be5965f85c30037a749bdc
Krish Prabhu <prabhukrish710@gmail.com>
2025-04-14 18:59:37 -0500

    maybe this works

--- Pi/piLight.py
+++ Pi/piLight.py
@@ -1,6 +1,17 @@
-from gpiozero import *
+import RPi.GPIO as GPIO
 import time
 
-led = LED(17)
+# Set the GPIO mode (BCM uses the GPIO numbers, not pin numbers)
+GPIO.setmode(GPIO.BCM)
 
-led.on()
+# Set up the LED pin (GPIO 17)
+led_pin = 17
+GPIO.setup(led_pin, GPIO.OUT)
+
+# Turn on the LED
+GPIO.output(led_pin, GPIO.HIGH)
+
+time.sleep(10)
+
+# Always clean up at the end of your script
+GPIO.cleanup()
