09d3784ae5bbec1184db793671ac8b0af3a96fec
Krish Prabhu <prabhukrish710@gmail.com>
2025-04-14 19:50:38 -0500

    tweaks to system file

--- Pi/piLight.py
+++ Pi/piLight.py
@@ -19,17 +19,19 @@ def main():
     # Get the command (0 or 1)
     command = sys.argv[1]
     
-    if command == "1":
+    if command == "1" or command.lower() == "on":
         # Turn on the LED
         GPIO.output(led_pin, GPIO.HIGH)
         print("LED turned ON")
-    elif command == "0":
+    elif command == "0" or command.lower() == "off":
         # Turn off the LED
         GPIO.output(led_pin, GPIO.LOW)
         print("LED turned OFF")
         GPIO.cleanup()
+    elif command == "help":
+        print("1/on : Turn on RGB light\n0/off : Turn off RGB light")
     else:
-        print("Invalid command. Use 0 to turn off or 1 to turn on.")
+        print("Invalid command. Use 0/off to turn off or 1/on to turn on.")
         sys.exit(1)
 
 if __name__ == "__main__":
