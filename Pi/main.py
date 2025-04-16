1410eb92e0840ac1ad65cdd2e3b696ddbbe6a1a6
Krish Prabhu <prabhukrish710@gmail.com>
2025-04-16 17:38:02 -0500

    small things before switching to ubuntu as windows is shit

--- Pi/main.py
+++ Pi/main.py
@@ -2,13 +2,13 @@ import transmission
 from jerial import JSONSerialReader
 import time
 
+'''
+TODO: Check to see if the usb ports on the Raspberry Pi are still not working with tty.
+'''
+
 pico = JSONSerialReader("/dev/pico")
 arduino = JSONSerialReader("/dev/arduino")
 
-while True:
-    pico.poll()
-    arduino.poll()
-    print(f"Pico: {pico.get_latest()}")
-    print(f"Arduino: {arduino.get_latest()}")
-    pico.send({"command" : "poll"})
-    arduino.send({"command" : "poll"})
+
+if __name__ == "__main__":
+    print("Booting up system.")
